c4d38bf5eadbc1efedb11cd22f0ff5296bf97678
정도훈 <harryjdh00@gmail.com>
2022-01-16 20:28:22 +0900

    시작페이지

--- lib/main.dart
+++ lib/main.dart
@@ -4,18 +4,133 @@ import 'package:mosigg/signup1.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({ Key? key }) : super(key: key);
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'MOSIGG',
-      theme: ThemeData(fontFamily: 'NotoSans', primaryColor: Colors.white, scaffoldBackgroundColor: Colors.white),
+      theme: ThemeData(
+          fontFamily: 'NotoSans',
+          primaryColor: Colors.white,
+          scaffoldBackgroundColor: Colors.white),
       debugShowCheckedModeBanner: false,
-      initialRoute: '/',
-      routes: {
-        '/': (context) => SignUp1()
-      },
+      home: StartPage(),
+    );
+  }
+}
+
+class StartPage extends StatelessWidget {
+  const StartPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xff001A5D),
+      body: Builder(
+        builder: (context) {
+          return SingleChildScrollView(
+            child: Column(
+              children: [
+                Padding(
+                  padding: EdgeInsets.only(top: 200),
+                ),
+                Center(
+                  child: Column(
+                    children: [
+                      Text(
+                        'MOSSIGG',
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 54,
+                            fontWeight: FontWeight.bold),
+                      ),
+                      Text(
+                        '차량 관리를 더 쉽고 더 간편하게',
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 14,
+                            fontWeight: FontWeight.w500),
+                      ),
+                      SizedBox(
+                        height: 200,
+                      ),
+                      SizedBox(
+                        width: 270,
+                        height: 45,
+                        child: ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                              primary: Color(0xfff9e000),
+                              onPrimary: Color(0xff000000)),
+                          onPressed: () {/*카카오톡 로그인 페이지*/},
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Icon(Icons.chat_bubble, color: Colors.black),
+                              SizedBox(
+                                width: 10,
+                              ),
+                              Text(
+                                '카카오톡으로 로그인',
+                                style: TextStyle(
+                                    color: Colors.black,
+                                    fontSize: 14,
+                                    fontWeight: FontWeight.w500),
+                              )
+                            ],
+                          ),
+                        ),
+                      ),
+                      SizedBox(height: 12),
+                      SizedBox(
+                        width: 270,
+                        height: 45,
+                        child: ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                              primary: Color(0xffffffff),
+                              onPrimary: Color(0xff000000)),
+                          onPressed: () {/*모시깽이로 로그인 페이지로*/},
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Text(
+                                '모시깽이로 로그인',
+                                style: TextStyle(
+                                    color: Colors.black,
+                                    fontSize: 14,
+                                    fontWeight: FontWeight.w500),
+                              )
+                            ],
+                          ),
+                        ),
+                      ),
+                      TextButton(
+                        style: TextButton.styleFrom(
+                          primary: Color(0xff000000),
+                        ),
+                        onPressed: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(builder: (context) => SignUp1()),
+                          );
+                        },
+                        child: Text(
+                          '회원가입',
+                          style: TextStyle(
+                            color: Color(0xffffffff),
+                            fontSize: 14,
+                            fontWeight: FontWeight.w500,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                )
+              ],
+            ),
+          );
+        },
+      ),
     );
   }
 }
--- lib/signup1.dart
+++ lib/signup1.dart
@@ -3,25 +3,24 @@ import 'package:fluttertoast/fluttertoast.dart';
 import 'package:mosigg/signup2.dart';
 
 class SignUp1 extends StatefulWidget {
-  const SignUp1({ Key? key }) : super(key: key);
+  const SignUp1({Key? key}) : super(key: key);
 
   @override
   _SignUpState createState() => _SignUpState();
 }
 
 class _SignUpState extends State<SignUp1> {
-
   final inputId = TextEditingController();
-  String id='';
+  String id = '';
   var idExist = false;
 
   @override
-  void initState(){
+  void initState() {
     super.initState();
   }
 
   @override
-  void dispose(){
+  void dispose() {
     inputId.dispose();
     super.dispose();
   }
@@ -36,34 +35,39 @@ class _SignUpState extends State<SignUp1> {
         leading: IconButton(
           icon: Icon(Icons.clear_rounded),
           iconSize: 45.0,
-          onPressed: (){
+          onPressed: () {
             // 시작페이지로 이동
-          },),
+          },
+        ),
       ),
       body: Builder(
-        builder: (context){
+        builder: (context) {
           return GestureDetector(
-            onTap: (){
+            onTap: () {
               FocusScope.of(context).unfocus();
             },
             child: SingleChildScrollView(
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  Divider(thickness: 2.0, color: Color(0xff001A5D), endIndent: 300.0),
+                  Divider(
+                      thickness: 2.0,
+                      color: Color(0xff001A5D),
+                      endIndent: 300.0),
                   SizedBox(height: 40.0),
                   Padding(
-                    padding: EdgeInsets.only(left: 25.0),
-                    child: text('모시깽이의', 24.0, FontWeight.w600, Colors.black)
-                  ),
+                      padding: EdgeInsets.only(left: 25.0),
+                      child:
+                          text('모시깽이의', 24.0, FontWeight.w600, Colors.black)),
                   Padding(
-                    padding: EdgeInsets.only(left: 25.0),
-                    child: text('계정을 만들게요:)', 24.0, FontWeight.w600, Colors.black)
-                  ),
+                      padding: EdgeInsets.only(left: 25.0),
+                      child: text(
+                          '계정을 만들게요:)', 24.0, FontWeight.w600, Colors.black)),
                   SizedBox(height: 40.0),
                   Form(
                     child: Container(
-                      margin: EdgeInsets.symmetric(vertical: 0.0, horizontal: 25.0),
+                      margin:
+                          EdgeInsets.symmetric(vertical: 0.0, horizontal: 25.0),
                       child: Column(
                         crossAxisAlignment: CrossAxisAlignment.start,
                         children: [
@@ -71,25 +75,27 @@ class _SignUpState extends State<SignUp1> {
                           TextField(
                             controller: inputId,
                             decoration: InputDecoration(
-                              enabledBorder: UnderlineInputBorder(
-                                borderSide: BorderSide(color: Color(0xffCBCBCB), width: 2.0)
-                              ),
-                              focusedBorder: UnderlineInputBorder(
-                                borderSide: BorderSide(color: Color(0xffCBCBCB), width: 2.0)
-                              ),
-                              helperText: '아이디를 입력해주세요!',
-                              helperStyle: TextStyle(fontSize: 14.0, fontWeight: FontWeight.w600, color: Color(0xffCBCBCB)),
-                              focusColor: Colors.grey
-                            ),
+                                enabledBorder: UnderlineInputBorder(
+                                    borderSide: BorderSide(
+                                        color: Color(0xffCBCBCB), width: 2.0)),
+                                focusedBorder: UnderlineInputBorder(
+                                    borderSide: BorderSide(
+                                        color: Color(0xffCBCBCB), width: 2.0)),
+                                helperText: '아이디를 입력해주세요!',
+                                helperStyle: TextStyle(
+                                    fontSize: 14.0,
+                                    fontWeight: FontWeight.w600,
+                                    color: Color(0xffCBCBCB)),
+                                focusColor: Colors.grey),
                             keyboardType: TextInputType.emailAddress,
                           ),
                           SizedBox(height: 20.0),
                           ElevatedButton(
-                            onPressed: (){
+                            onPressed: () {
                               // 아이디 존재여부 확인
-                              if(inputId.text==id){
+                              if (inputId.text == id) {
                                 setState(() {
-                                  idExist=true;
+                                  idExist = true;
                                 });
                               }
                               // 없는 아이디면 서버에 데이터 전달 후 다음 페이지로 이동
@@ -97,56 +103,73 @@ class _SignUpState extends State<SignUp1> {
                                 setState(() {
                                   id = inputId.text;
                                 });
-                                idExist=false;
-                                Navigator.push(context, MaterialPageRoute(builder: (BuildContext context)=>SignUp2()));
+                                idExist = false;
+                                Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                        builder: (BuildContext context) =>
+                                            SignUp2()));
                               }
                             },
-                            child: text('계속하기', 14.0, FontWeight.w600, Colors.white),
+                            child: text(
+                                '계속하기', 14.0, FontWeight.w600, Colors.white),
                             style: ElevatedButton.styleFrom(
-                              primary: Color(0xff001A5D),
-                              shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(5.0)
-                              ),
-                              elevation: 0.0,
-                              padding: EdgeInsets.symmetric(vertical: 12.0, horizontal: 154.7)
-                            ),
+                                primary: Color(0xff001A5D),
+                                shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(5.0)),
+                                elevation: 0.0,
+                                padding: EdgeInsets.symmetric(
+                                    vertical: 12.0, horizontal: 154.7)),
                           ),
-                          (idExist==true) ? 
-                              Center(child: Padding(
-                                padding: const EdgeInsets.all(20.0),
-                                child: Text('앗! 이미 존재하는 아이디예요!', style: TextStyle(fontSize: 14.0, fontWeight: FontWeight.w600, color: Colors.black)),
-                              )) : SizedBox(height: 61.0),
+                          (idExist == true)
+                              ? Center(
+                                  child: Padding(
+                                  padding: const EdgeInsets.all(20.0),
+                                  child: Text('앗! 이미 존재하는 아이디예요!',
+                                      style: TextStyle(
+                                          fontSize: 14.0,
+                                          fontWeight: FontWeight.w600,
+                                          color: Colors.black)),
+                                ))
+                              : SizedBox(height: 61.0),
                           SizedBox(height: 90.0),
                           Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              Container(height: 1.0, width: 130.0, color: Color(0xffCBCBCB)),
-                              TextButton(
-                                onPressed: (){
-                                  // 간편 회원가입 페이지로 이동
-                                }, 
-                                child: text('간편 회원가입', 14.0, FontWeight.w600, Color(0xffCBCBCB))
-                              ),
-                              Container(height: 1.0, width: 130.0, color: Color(0xffCBCBCB)),
-                            ]
-                          ),
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Container(
+                                    height: 1.0,
+                                    width: 130.0,
+                                    color: Color(0xffCBCBCB)),
+                                TextButton(
+                                    onPressed: () {
+                                      // 간편 회원가입 페이지로 이동
+                                    },
+                                    child: text('간편 회원가입', 14.0,
+                                        FontWeight.w600, Color(0xffCBCBCB))),
+                                Container(
+                                    height: 1.0,
+                                    width: 130.0,
+                                    color: Color(0xffCBCBCB)),
+                              ]),
                           InkWell(
-                            onTap: (){
+                            onTap: () {
                               // 카톡 로그인 페이지로 이동
                             },
                             child: Container(
-                              padding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 120.0),
-                              decoration: BoxDecoration(
-                                color: Color(0xffF9E000),
-                                borderRadius: BorderRadius.circular(5.0)
-                              ),
-                              child: Row(
-                                children: [
-                                  Image.asset('image/kakao.png', width: 25, height: 25),
-                                  SizedBox(width: 5),
-                                  text('카카오로 시작', 14.0, FontWeight.w600, Colors.black)
-                                ],
-                              )),
+                                padding: EdgeInsets.symmetric(
+                                    vertical: 10.0, horizontal: 120.0),
+                                decoration: BoxDecoration(
+                                    color: Color(0xffF9E000),
+                                    borderRadius: BorderRadius.circular(5.0)),
+                                child: Row(
+                                  children: [
+                                    Image.asset('image/kakao.png',
+                                        width: 25, height: 25),
+                                    SizedBox(width: 5),
+                                    text('카카오로 시작', 14.0, FontWeight.w600,
+                                        Colors.black)
+                                  ],
+                                )),
                           ),
                         ],
                       ),
@@ -162,8 +185,7 @@ class _SignUpState extends State<SignUp1> {
   }
 }
 
-Text text(content, size, weight, colors){
-  return Text(
-    content, style: TextStyle(fontSize: size, fontWeight: weight, color: colors)
-  );
+Text text(content, size, weight, colors) {
+  return Text(content,
+      style: TextStyle(fontSize: size, fontWeight: weight, color: colors));
 }
