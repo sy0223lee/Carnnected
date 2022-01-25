930cf1e59703c35fa5366caeb2a88ce5a4316c0f
sy0223lee <sy0223lee@gmail.com>
2022-01-26 02:34:37 +0900

    add reservation tables-1

--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -9,15 +9,48 @@ CREATE TABLE `MEMBER`(
     `phone` CHAR(11) NOT NULL,
     PRIMARY KEY (`id`));
 
-INSERT INTO `MEMBER` VALUES
-('dlekdud0102','password1234','이다영','010102','01012345678'),
-('mouse0429','password1234','김민지','000429','01023456789'),
-('sy02lee','password1234','이승윤','000223','01034567890'),
-('harrydh','password1234','정도훈','000601','01045678901'),
-('lseyeon0723','password1234','이세연','990326','01056789012'),
-('baekhyuneeexo','password1234','변백현','920506','01067890123'),
-('dlwlrma','password1234','이지은','930516','01078901234'),
-('beeeestdjdjdj','password1234','윤두준','890704','01089012345'),
-('yysbeast','password1234','양요섭','900105','01090123456'),
-('gttk0000','password1234','이기광','900330','01001234567'),
-('highlightdnpn','password1234','손동운','910606','01089756421');
+# 주유 서비스 예약
+CREATE TABLE `GAS_RESRV`(
+	`id` VARCHAR(15) NOT NULL,
+    `time` DATETIME NOT NULL,
+    `type` VARCHAR(10) NOT NULL,
+    `amount` INT NOT NULL,
+    `source` VARCHAR(50) NOT NULL,
+    `dest_name` VARCHAR(15) NOT NULL,
+    `dest_addr` VARCHAR(50) NOT NULL,
+    `price` INT NOT NULL,
+    FOREIGN KEY (`id`)
+    REFERENCES `MEMBER`(`id`) ON UPDATE CASCADE);
+
+# 세차 서비스 예약    
+CREATE TABLE `WASH_RESRV`(
+	`id` VARCHAR(15) NOT NULL,
+    `time` DATETIME NOT NULL,
+    `type` VARCHAR(10) NOT NULL,
+    `company` VARCHAR(15) NOT NULL,
+    `source` VARCHAR(50) NOT NULL,
+    `price` INT NOT NULL,
+    FOREIGN KEY (`id`)
+    REFERENCES `MEMBER`(`id`) ON UPDATE CASCADE);
+
+# 딜리버리 서비스 예약
+CREATE TABLE `DELIV_RESRV`(
+	`id` VARCHAR(15) NOT NULL,
+    `time` DATETIME NOT NULL,
+    `source` VARCHAR(50) NOT NULL,
+    `dest_name` VARCHAR(15),
+    `dest_addr` VARCHAR(50) NOT NULL,
+    `price` INT NOT NULL,
+    FOREIGN KEY (`id`)
+    REFERENCES `MEMBER`(`id`) ON UPDATE CASCADE);   
+
+# 대리운전 서비스 예약    
+CREATE TABLE `DRIVE_RESRV`(
+	`id` VARCHAR(15) NOT NULL,
+    `time` DATETIME NOT NULL,
+    `source` VARCHAR(50) NOT NULL,
+    `dest_name` VARCHAR(15),
+    `dest_addr` VARCHAR(50) NOT NULL,
+    `price` INT NOT NULL,
+    FOREIGN KEY (`id`)
+    REFERENCES `MEMBER`(`id`) ON UPDATE CASCADE);  
