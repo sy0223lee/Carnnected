eb449e610c65cd6a500e021ca417cbd0fdf1ce21
sy0223lee <sy0223lee@gmail.com>
2022-02-07 22:16:49 +0900

    추가요청사항 수정

--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -15,7 +15,7 @@ CREATE TABLE `CAR`(
 	`id` VARCHAR(15) NOT NULL,			# 회원 id
     `model` VARCHAR(20) NOT NULL,		# 차종
     `year` INT NOT NULL,				# 차 연식
-    `number` CHAR(8) NOT NULL,			# 차 번호
+    `number` CHAR(11) NOT NULL,			# 차 번호
     #`imei` INT NOT NULL,				# 이동 단말 위치 추적 가능한 그런건가봐 아이카에 준 데이터에 있길래 추가해놓음
     `name` VARCHAR(15) NULL,			# 차 별명
     `image` VARCHAR(20) NOT NULL,		# 차 사진 경로
@@ -26,7 +26,7 @@ CREATE TABLE `CAR`(
 
 # 차량 상태 정보 테이블
 CREATE TABLE `CAR_STATE`(
-	`number` CHAR(8) NOT NULL,			# 차 번호
+	`number` CHAR(11) NOT NULL,			# 차 번호
     `left_fuel` INT,					# 남은 연료
     `gps` INT,							# 차량 위치
     FOREIGN KEY(`index`)
@@ -35,13 +35,13 @@ CREATE TABLE `CAR_STATE`(
 # 주유 서비스 예약
 CREATE TABLE `GAS_RESRV`(
 	`id` VARCHAR(15) NOT NULL,			# 회원 id
-    `number` CHAR(8) NOT NULL,			# 차 번호
+    `number` CHAR(11) NOT NULL,			# 차 번호
     `time` DATETIME NOT NULL,			# 예약 날짜, 시간
-    `type` VARCHAR(10) NOT NULL,		# 연료 종류
+    `type` VARCHAR(20) NOT NULL,		# 연료 종류
     `amount` INT NOT NULL,				# 주유 금액
-    `source` VARCHAR(50) NOT NULL,		# 차량 위치
-    `dest_name` VARCHAR(15) NOT NULL,	# 주유소 이름
-    `dest_addr` VARCHAR(50) NOT NULL,	# 주유소 주소
+    `source` VARCHAR(100) NOT NULL,		# 차량 위치
+    `dest_name` VARCHAR(30) NOT NULL,	# 주유소 이름
+    `dest_addr` VARCHAR(100) NOT NULL,	# 주유소 주소
     `price` INT NOT NULL,				# 예상 가격
     FOREIGN KEY (`id`)
     REFERENCES `MEMBER`(`id`) ON UPDATE CASCADE);
@@ -49,12 +49,11 @@ CREATE TABLE `GAS_RESRV`(
 # 세차 서비스 예약    
 CREATE TABLE `WASH_RESRV`(
 	`id` VARCHAR(15) NOT NULL,			# 회원 id
-    `number` CHAR(8) NOT NULL,			# 차 번호
+    `number` CHAR(11) NOT NULL,			# 차 번호
     `time` DATETIME NOT NULL,			# 예약 날짜, 시간
-    `type` VARCHAR(10) NOT NULL,		# 세차 종류
     `detail` VARCHAR(600),				# 추가 요청 사항
-    `company` VARCHAR(15) NOT NULL,		# 세차 업체
-    `source` VARCHAR(50) NOT NULL,		# 차량 위치
+    `type` VARCHAR(30) NOT NULL,		# 세차 종류
+    `source` VARCHAR(100) NOT NULL,		# 차량 위치
     `price` INT NOT NULL,				# 예상 가격
     FOREIGN KEY (`id`)
     REFERENCES `MEMBER`(`id`) ON UPDATE CASCADE);
@@ -62,12 +61,12 @@ CREATE TABLE `WASH_RESRV`(
 # 딜리버리 서비스 예약
 CREATE TABLE `DELIV_RESRV`(
 	`id` VARCHAR(15) NOT NULL,			# 회원 id
-    `number` CHAR(8) NOT NULL,			# 차 번호
+    `number` CHAR(11) NOT NULL,			# 차 번호
     `time` DATETIME NOT NULL,			# 예약 날짜, 시간
     `detail` VARCHAR(600),				# 추가 요청 사항
-    `source` VARCHAR(50) NOT NULL,		# 차량 위치
-    `dest_name` VARCHAR(15),			# 목적지 이름
-    `dest_addr` VARCHAR(50) NOT NULL,	# 목적지 주소
+    `source` VARCHAR(100) NOT NULL,		# 차량 위치
+    `dest_name` VARCHAR(30),			# 목적지 이름
+    `dest_addr` VARCHAR(100) NOT NULL,	# 목적지 주소
     `price` INT NOT NULL,				# 예상 가격
     FOREIGN KEY (`id`)
     REFERENCES `MEMBER`(`id`) ON UPDATE CASCADE);   
@@ -75,11 +74,11 @@ CREATE TABLE `DELIV_RESRV`(
 # 대리운전 서비스 예약    
 CREATE TABLE `DRIVE_RESRV`(
 	`id` VARCHAR(15) NOT NULL,			# 회원 id
-    `number` CHAR(8) NOT NULL,			# 차 번호
+    `number` CHAR(11) NOT NULL,			# 차 번호
     `time` DATETIME NOT NULL,			# 예약 날짜, 시간
-    `source` VARCHAR(50) NOT NULL,		# 차량 위치
-    `dest_name` VARCHAR(15),			# 목적지 이름
-    `dest_addr` VARCHAR(50) NOT NULL,	# 목적지 주소
+    `source` VARCHAR(100) NOT NULL,		# 차량 위치
+    `dest_name` VARCHAR(30),			# 목적지 이름
+    `dest_addr` VARCHAR(100) NOT NULL,	# 목적지 주소
     `price` INT NOT NULL,				# 예상 가격
     FOREIGN KEY (`id`)
     REFERENCES `MEMBER`(`id`) ON UPDATE CASCADE);  
@@ -87,25 +86,25 @@ CREATE TABLE `DRIVE_RESRV`(
 # 방문교체 서비스 예약    
 CREATE TABLE `REPLACE_RESRV`(
 	`id` VARCHAR(15) NOT NULL,			# 회원 id
-    `number` CHAR(8) NOT NULL,			# 차 번호
+    `number` CHAR(11) NOT NULL,			# 차 번호
     `time` DATETIME NOT NULL,			# 예약 날짜, 시간
-	`item` VARCHAR(20) NOT NULL,		# 교체 용품
+	`item` VARCHAR(30) NOT NULL,		# 교체 용품
 	`detail` VARCHAR(600),				# 추가 요청 사항
-    `source` VARCHAR(50) NOT NULL,		# 차량 위치
+    `source` VARCHAR(100) NOT NULL,		# 차량 위치
     `price` INT NOT NULL,				# 예상 가격
     FOREIGN KEY (`id`)
     REFERENCES `MEMBER`(`id`) ON UPDATE CASCADE);
     
-# 대리정비 서비스 예약
+# 정비 서비스 예약
 CREATE TABLE `REPAIR_RESRV`(
 	`id` VARCHAR(15) NOT NULL,			# 회원 id
-    `number` CHAR(8) NOT NULL,			# 차 번호
+    `number` CHAR(11) NOT NULL,			# 차 번호
     `time` DATETIME NOT NULL,			# 예약 날짜, 시간
-    `type` VARCHAR(10) NOT NULL,		# 검사 종류
+    `type` VARCHAR(30) NOT NULL,		# 검사 종류
     `detail` VARCHAR(600),				# 추가 요청 사항
-    `source` VARCHAR(50) NOT NULL,		# 차량 위치
-    `dest_name` VARCHAR(15) NOT NULL,	# 정비소 이름
-    `dest_addr` VARCHAR(50) NOT NULL,	# 정비소 주소
+    `source` VARCHAR(100) NOT NULL,		# 차량 위치
+    `dest_name` VARCHAR(30) NOT NULL,	# 정비소 이름
+    `dest_addr` VARCHAR(100) NOT NULL,	# 정비소 주소
     `price` INT NOT NULL,				# 예상 가격
     FOREIGN KEY (`id`)
     REFERENCES `MEMBER`(`id`) ON UPDATE CASCADE);  
